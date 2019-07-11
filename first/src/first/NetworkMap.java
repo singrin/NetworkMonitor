75bdbdefb2ec4d5d4391b8594ec8a5d0eb3b9ab9
singrin <rohitduggal99@gmail.com>
2019-07-11 18:32:41 +0530

    mobile jbutton
    11/07/19
    mobile jbutton added

--- first/src/first/NetworkMap.java
+++ first/src/first/NetworkMap.java
@@ -45,6 +45,7 @@ public class NetworkMap extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 800, 600);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(240, 240, 240));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
@@ -62,11 +63,30 @@ public class NetworkMap extends JFrame {
 		});
 		btnHome.setBounds(685, 513, 89, 23);
 		contentPane.add(btnHome);
+		JButton btnNewButton = new JButton("Mobile");
+		btnNewButton.setBackground(Color.GREEN);
+		btnNewButton.setVisible(false);
+		btnNewButton.setIcon(new ImageIcon(NetworkMap.class.getResource("/javax/swing/plaf/metal/icons/ocean/computer.gif")));
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JTextPane txtpnMobileTejaswis = new JTextPane();
+				txtpnMobileTejaswis.setVisible(true);
+				txtpnMobileTejaswis.setBackground(SystemColor.control);
+				txtpnMobileTejaswis.setText("172.20.10.3\r\nMobile\r\nTejaswi's Mobile");
+				txtpnMobileTejaswis.setBounds(577, 203, 100, 57);
+				contentPane.add(txtpnMobileTejaswis);
+				
+			}
+		});
+		btnNewButton.setBounds(567, 155, 105, 49);
+		contentPane.add(btnNewButton);
 		
 		JButton btnHome_1 = new JButton("Home Server");
 		btnHome_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btnHome_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				btnNewButton.setVisible(true);
 			}
 		});
 		btnHome_1.setIcon(new ImageIcon(NetworkMap.class.getResource("/javax/swing/plaf/metal/icons/ocean/directory.gif")));
@@ -83,28 +103,14 @@ public class NetworkMap extends JFrame {
 		
 		JTextPane txtpnMobileRohits = new JTextPane();
 		txtpnMobileRohits.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		txtpnMobileRohits.setBackground(SystemColor.menu);
+		txtpnMobileRohits.setBackground(SystemColor.control);
 		txtpnMobileRohits.setText("172.20.10.1\r\nMobile\r\nRohit's Mobile\r\n");
 		txtpnMobileRohits.setBounds(358, 318, 89, 49);
 		contentPane.add(txtpnMobileRohits);
 		
-		JButton btnNewButton = new JButton("Mobile");
-		btnNewButton.setBackground(Color.GREEN);
-		btnNewButton.setVisible(false);
-		btnNewButton.setIcon(new ImageIcon(NetworkMap.class.getResource("/javax/swing/plaf/metal/icons/ocean/computer.gif")));
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewButton.setBounds(567, 155, 105, 49);
-		contentPane.add(btnNewButton);
 		
-		JTextPane txtpnMobileTejaswis = new JTextPane();
-		txtpnMobileTejaswis.setBackground(SystemColor.menu);
-		txtpnMobileTejaswis.setText("172.20.10.3\r\nMobile\r\nTejaswi's Mobile");
-		txtpnMobileTejaswis.setBounds(577, 203, 95, 57);
-		contentPane.add(txtpnMobileTejaswis);
+		
+		
 
 		
 	}
